5c5ac6b2b6ab1518d3dcf5646b2503ae14911b8e
VishwajeetJadeja <40612512+VishwajeetJadeja@users.noreply.github.com>
2022-05-08 21:36:15 -0500

    Add files via upload

--- bigNumberComputation.java
+++ bigNumberComputation.java
@@ -3,6 +3,7 @@ package com.bigNumberComputation;
 import java.util.*;
 import java.io.*;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 
 public class bigNumberComputation {
@@ -12,14 +13,21 @@ public class bigNumberComputation {
 			
 		String input1 = "";
 		String input2 = "";
+		String input3 = "";
+		String input4 = "";
 		
 		  BigDecimal sum;
+		  
+		  //BigInteger for multiplication
+		  BigInteger mult;
+	 
+		  
 		  Scanner scan = new Scanner(System.in);
-		  System.out.println("Enter first BigNumber:");
+		  System.out.println("Enter first BigNumber for addition:");
 		  
 		  input1 = scan.nextLine();
 		  
-		  System.out.println("Enter second BigNumber:");
+		  System.out.println("Enter second BigNumber for addition:");
 		  
 		  input2 = scan.nextLine();
 		  
@@ -48,6 +56,38 @@ public class bigNumberComputation {
 			  System.out.println("Invalid input");
 			  
 		  }
+		  
+		  System.out.println("Enter first BigNumber for multiplication:");
+		  
+		  input3 = scan.nextLine();
+		  
+		  System.out.println("Enter second BigNumber for multiplication");
+		  
+		  input4 = scan.nextLine();
+		  
+		  
+		 /*
+		  input3 = 123456789;
+		  input4 = 987654321;
+		  
+		  */
+		  
+		  
+		  if(input3!="" && input4!="") {
+			  
+			  BigInteger c = new BigInteger(input3);	  
+			  BigInteger d = new BigInteger(input4);
+			  
+			mult = c.multiply(d);
+			
+		  System.out.println("The product of\n"
+                  + c + "x" + d + " "
+                  + "is " + mult + "\n");
+		  
+		  } else {
+			  
+			  System.out.println("Invalid input");
+			  }
 		  	
 	}
 
